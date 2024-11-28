87df6159b3c5b6b46756f56600f026c13e4ad206
@Rewan-Adel <rewanadel1266@gmail.com>
2024-11-28 23:09:21 +0200

    Logs

--- src/logger.ts
+++ src/logger.ts
@@ -3,20 +3,20 @@ import winston from "winston";
 const logFormat = winston.format.combine( 
     winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
+
+    winston.format.printf(({timestamp,level,message})=>{
         return `${timestamp} ${level} ${message}`;
     })
 );
 
 const Logger = winston.createLogger({
-    level: "info",
+    level : "info",
     format: logFormat,
     transports: [
         new winston.transports.Console(),
-        ...(process.env.NODE_ENV !== "production" // Disable file logging in production
-            ? [new winston.transports.File({ filename: "logs/app.log" })] 
-            : [])
+        //new winston.transports.File({filename: "logs/app.log"}),
     ]
 });
+
 
 export default Logger;
